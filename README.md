32af0404960ee9f918dfa2373007a6339768f069
leosanchez <slash902daniel@gmail.com>
2022-10-06 20:46:39 -0500

    ADD functions as first class citizens

---
+++ README.md
@@ -0,0 +1,7 @@
+# BASED IN TUTORIAL
+https://www.youtube.com/watch?v=poQXNp9ItL4
+
+## HOW TO RUN SERVER
+npm start
+
+## HOW TO...
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+//FUNTIONS AS FRIST CLASS CITIZENS
+//as can be treated as any other variable
+
+//VERSION 1 ----------------------------------------------------------
+function sayHello() {
+        return 'Hello World';
+}
+
+//Set reference or alias
+let fn = sayHello;
+
+//fn() or  sayHello()   is the same to call
+
+function greet(fnMessage) {
+    console.log(fnMessage());
+}
+
+greet(sayHello);
+
+//VERSION 2 ----------------------------------------------------------
+function sayHello2() {
+    return function () {
+        return 'Hello World';
+    };
+}
+
+let fn2 = sayHello2();
+let message = fn2();
+
+
