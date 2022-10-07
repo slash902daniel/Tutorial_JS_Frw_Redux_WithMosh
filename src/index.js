14cf1e546e346643106cb9cce24738dc4616f68d
leosanchez <slash902daniel@gmail.com>
2022-10-06 21:15:27 -0500

    Add functional composition

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+//FUNCTIONAL PROGRAMMING
+
+/* MAIN CONCEPTS
+1) Functions as first class citizens
+2) High Order functions
+3) Function Composition
+*/
+
+
+//==========================================================================================
 //FUNTIONS AS FRIST CLASS CITIZENS
 //as can be treated as any other variable
 
-//VERSION 1 ----------------------------------------------------------
+//FUNTIONS AS FRIST CLASS CITIZENS - VERSION 1 ----------------------------------------------------------
 function sayHello() {
         return 'Hello World';
 }
@@ -17,7 +27,7 @@ function greet(fnMessage) {
 
 greet(sayHello);
 
-//VERSION 2 ----------------------------------------------------------
+//FUNTIONS AS FRIST CLASS CITIZENS - VERSION 2 ----------------------------------------------------------
 function sayHello2() {
     return function () {
         return 'Hello World';
@@ -26,5 +36,48 @@ function sayHello2() {
 
 let fn2 = sayHello2();
 let message = fn2();
+
+//==========================================================================================
+//HIGH ORDER FUNCTIONS
+//Is a function that takes a function as arg or returns it or both.
+
+//HIGH ORDER FUNCTIONS - VERSION 1 ----------------------------------------------------------
+function greet(fn) {
+    console.log(fn());
+}
+
+function sayHello3() {
+    return function () {
+           return 'Hello World';
+   };
+}
+
+//Examples of common usage
+
+let numbers = [1,2,3];
+numbers.map(number => number * 2);
+
+setTimeout(() => console.log('hello pausing'), 1000);
+
+//==========================================================================================
+//Function composition
+//Is a function 
+
+//FUNCTION COMPOSITION - VERSION 1 ----------------------------------------------------------
+
+//None functional example
+let input = '   Javascript   '
+let output = '<div>' + input.trim() + '</div>'
+
+//example functional
+const trim = str = str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+wrapInDiv(toLowerCase(trim(input)));
+//See above.. does the stuff but to confussing and many () to mantain
+
+//FUNCTION COMPOSITION - VERSION 2 WITH LODASH ----------------------------------------------------------
+
 
 
